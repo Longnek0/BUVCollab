a8613a2993101b056e1c40500a12f3e8dd206d41
Longnek0 <hunglong2000101@gmail.com>
2021-11-18 16:11:25 +0700

    Add project to github

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public InventoryObj inventory;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponent<GroundItem>();
+        if (item)
+        {
+            inventory.AddItem(new Item(item.item), 1);
+            Destroy(other.gameObject);
+        }
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            inventory.Save();
+        }
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            inventory.Load();
+        }
+    }
+    public void OnApplicationQuit()
+    {
+        inventory.Container.Items = new InventorySlot[20];
+    }
+}
